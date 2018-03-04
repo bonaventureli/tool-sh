27cdc15d632bc454239114cb4bb367d3fd4b3861
bonaventureli <lfwendula@msn.cn>
2018-03-04 00:43:53 -0800

    opengl

---
+++ DIYsystem1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+downarm-linux-gnueabihf-6.4.1.sh
+makeinstalluboot.sh
+downlinuxkernel.sh
+makeinstalllinuxkernel.sh
+debian9.3.sh
+
---
+++ makeinstallqt4.8.6-opengl-ubuntu.sh
@@ -0,0 +1,1 @@
+./configure -prefix  /usr/local/Trolltech/QtEmbedded-4.8.6-ubuntu \
