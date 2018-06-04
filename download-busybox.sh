32f4eb3d87f9b796ea5fba96960d173037a3a9c8
bonaventureli <lfwendula@msn.cn>
2018-06-04 01:41:56 -0700

    busybox

---
+++ download-busybox.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wget -c https://busybox.net/downloads/busybox-1.28.4.tar.bz2
+wget -c https://busybox.net/downloads/busybox-1.27.2.tar.bz2
+wget -c https://busybox.net/downloads/busybox-1.26.2.tar.bz2
+wget -c https://busybox.net/downloads/busybox-1.25.1.tar.bz2
+wget -c https://busybox.net/downloads/busybox-1.24.2.tar.bz2
+wget -c https://busybox.net/downloads/busybox-1.23.2.tar.bz2
+wget -c https://busybox.net/downloads/busybox-1.22.1.tar.bz2
+wget -c https://busybox.net/downloads/busybox-1.21.1.tar.bz2
+wget -c https://busybox.net/downloads/busybox-1.20.2.tar.bz2
+
+#tar -xvf busybox-1.21.1.tar.bz2
+#make menuconfig
+#BuildOptiions_> Cross Compiler prefix->arm-none-linux-gnueabi-
+##Installation Options->BusyBox installation prefix->/./install
+#Installation Options->BusyBox installation prefix->/../system
+#make
+#make install
+
