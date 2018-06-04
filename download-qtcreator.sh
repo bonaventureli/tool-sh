8e7f70c38d2c960c7a8733391bf9fa029b963d88
bonaventureli <lfwendula@msn.cn>
2018-06-04 01:08:53 -0700

    qt

---
+++ download-qtcreator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/4.6/4.6.1/qt-creator-opensource-src-4.6.1.tar.xz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/4.5/4.5.2/qt-creator-opensource-src-4.5.2.tar.xz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/4.4/4.4.1/qt-creator-opensource-src-4.4.1.tar.xz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/4.3/4.3.1/qt-creator-opensource-src-4.3.1.tar.xz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/4.2/4.2.2/qt-creator-opensource-src-4.2.2.tar.xz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/4.1/4.1.0/qt-creator-opensource-src-4.1.0.tar.xz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/4.0/4.0.3/qt-creator-opensource-src-4.0.3.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/3.6/3.6.1/qt-creator-opensource-src-3.6.1.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/3.5/3.5.1/qt-creator-opensource-src-3.5.1.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/3.4/3.4.2/qt-creator-opensource-src-3.4.2.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/3.3/3.3.2/qt-creator-opensource-src-3.3.2.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/3.2/3.2.2/qt-creator-opensource-src-3.2.2.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/3.1/3.1.2/qt-creator-opensource-src-3.1.2.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/3.0/3.0.1/qt-creator-opensource-src-3.0.1.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/2.8/2.8.1/qt-creator-2.8.1-src.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/2.7/2.7.2/qt-creator-2.7.2-src.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/2.6/2.6.2/qt-creator-2.6.2-src.tar.gz
+wget -c http://mirrors.ustc.edu.cn/qtproject/official_releases/qtcreator/2.5/2.5.2/qt-creator-2.5.2-src.tar.gz
---
+++ makelinuxkernel4.16.12.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+case ${1} in
+	"make")
+	    tar xf gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf.tar.xz
+	    #tar xf gcc-linaro-6.4.1-2017.11-x86_64_arm-linux-gnueabihf.tar.xz
+	    export CC=`pwd`/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+	    #export CC=`pwd`/gcc-linaro-6.4.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+	    
+	    LINUXKERNELFILE=linux-stable-4.16.12.tar.gz
+	    LINUXFILE=linux-stable-4.16.12
+	    
+	    tar zxvf $LINUXKERNELFILE
+	    cd $LINUXFILE
+	    make ARCH=arm CROSS_COMPILE=${CC} distclean
+	    make ARCH=arm CROSS_COMPILE=${CC} omap3_evm_defconfig
+	    make ARCH=arm CROSS_COMPILE=${CC} menuconfig
+	    #make ARCH=arm CROSS_COMPILE=${CC} uImage modules
+	    make ARCH=arm CROSS_COMPILE=${CC} 
+	;;
+	"clean")
+	rm -rf gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf
+	rm -rf linux-stable-4.16.12
+	;;
+	*)
+	echo -e "\033[31m you must input ./${0} make/clean \033[0m"
+	;;
+esac
