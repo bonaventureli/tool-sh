20e35ad7faf97a0fde8e273ce0f5f7c91b248f54
bonaventureli <lfwendula@msn.cn>
2018-06-05 02:45:12 -0700

    firefox

---
+++ download-cc2540.pdf.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -c http://www.ti.com/lit/ug/swru191f/swru191f.pdf
+wget -c http://www.ti.com/lit/er/sprz360i/sprz360i.pdf
+#evince swru191f.pdf
---
+++ open-website.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#firefox www.baidu.com &&
+#firefox www.taobao.com &&
+#firefox www.ti.com &&
+#firefox www.teamviewer.com &&
+#firefox www.arduino.cc || exit 1
+
+#firefox www.baidu.com & www.taobao.com
+#firefox www.taobao.com &
+#firefox www.ti.com &
+#firefox www.teamviewer.com &
+#firefox www.arduino.cc || exit 1
+
+firefox www.baidu.com &
+firefox www.taobao.com &
+firefox www.ti.com &
+firefox www.teamviewer.com &
+firefox www.arduino.cc || exit 1
