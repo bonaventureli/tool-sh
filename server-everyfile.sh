5d8e3d9f6351915636b2fd4e6fea49083c64bc6c
bonaventureli <lfwendula@msn.cn>
2018-06-06 20:25:36 -0700

    server

--- server-everyfile.sh
+++ server-everyfile.sh
@@ -7,20 +7,22 @@ do
     do
 	if [ -f "$file" ]
 	then
-
+	    DIR=$PWD
 	    mkdir -p ../tmp-server
 	    cp email1.1.sh ../tmp-server
 	    cp name ../tmp-server
-	    cp pass ../tmp-server
+	    cp pass-a ../tmp-server
 	    cp $file ../tmp-server
+	    cd ../tmp-server
 	    starttime=`date +%Y-%m-%d-%H:%M:%S`
 	    LOGFILE=$file-$starttime.log
 	    SHELLFILE=$file
 	    LOGFILE=$SHELLFILE-$starttime.log
-	    (time /bin/bash -e ../tmp-server/$SHELLFILE) 2>&1 | tee $LOGFILE
+	    (time /bin/bash -e $PWD/$SHELLFILE) 2>&1 | tee $LOGFILE
 	    endtime=`date +%Y-%m-%d-%H:%M:%S`
 	    #echo $endtime 2>&1 | tee $LOGFILE
 	    /bin/bash $PWD/email1.1.sh $LOGFILE 
+	    cd $DIR
 	    rm -rf ../tmp-server
 	fi
     done
