852f80b093b7e63a30f1659fcba78b0df490af88
bonaventureli <lfwendula@msn.cn>
2018-06-12 18:49:39 -0700

    server

--- server-linuxkernel-beaglebone.sh
+++ server-linuxkernel-beaglebone.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get -y update
+sudo apt-get -y update || echo "some error"
 #sudo apt-get install flex
-sudo apt autoremove
-sudo apt-get install lzma lzop bison libncurses5-dev:amd64
+sudo apt autoremove || echo "some error"
+sudo apt-get install lzma lzop bison libncurses5-dev:amd64 || echo "some error"
 #wget -c https://jaist.dl.sourceforge.net/project/flexformatter/flexformatter/0.9.0/FlexPrettyPrintCommand_0.9.0.zip
 #wget -c http://down1.chinaunix.net/distfiles/flex-2.5.4a.tar.gz
 #vim /etc/apt/source.list
