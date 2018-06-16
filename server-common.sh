53b49a93c17e8cc1c06571fce8d1ff56fabaf383
bonaventureli <lfwendula@msn.cn>
2018-06-16 12:02:43 +0800

    server

--- server-common.sh
+++ server-common.sh
@@ -15,17 +15,15 @@ then
 	    while [ $i -lt 10 ]
 	    do
 	        starttime=`date +%Y-%m-%d-%H:%M:%S`
+	        #LOGFILE=${1}-$starttime.log
+	        #SHELLFILE=${1}
 	        LOGFILE=${1}-$starttime.log
-	        SHELLFILE=${1}
-	        LOGFILE=$SHELLFILE-$starttime.log
-	        #(time /bin/bash -e $PWD/$SHELLFILE ${2}) 2>&1 | tee $LOGFILE
-	        #(time /bin/bash -e $PWD/$SHELLFILE install) 2>&1 | tee $LOGFILE
-	        (time /bin/bash -e $PWD/$SHELLFILE) 2>&1 | tee $LOGFILE
+	        (time /bin/bash -e $PWD/${1}) 2>&1 | tee $LOGFILE
+	        #(time /bin/bash -e $PWD/$SHELLFILE) 2>&1 | tee $LOGFILE
 	        #endtime=`date +%Y-%m-%d-%H:%M:%S`
 		#echo $endtime 2>&1 | tee $LOGFILE
-	       # email1.1.sh $LOGFILE 
-	        /bin/bash $PWD/email1.1.sh $LOGFILE 
-	        rm *.log
+	        /bin/bash $PWD/email1.1.sh $LOGFILE && rm -rf *.log
+	        #rm *.log
 	       # rm -rf ../tmp-server
 	    done
 else
