01eb92ae46d2f05f7602923e7ee42319ad2e0841
bonaventureli <lfwendula@msn.cn>
2018-06-18 08:19:29 +0800

    bbb

--- server-linuxkernel-beaglebone.sh
+++ server-linuxkernel-beaglebone.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ "${1}" == "install" ]; then
 sudo apt-get -y update || echo "some error"
-#sudo apt-get install flex
 sudo apt autoremove || echo "some error"
 sudo apt-get install libssl-dev
+sudo apt-get install build-essential fakeroot flex
 sudo apt-get install lzma lzop bison libncurses5-dev:amd64 || echo "some error"
 elif [ "${1}" == "remove" ]; then
-sudo apt-get remove flex
-sudo apt autoremove || echo "some error"
+sudo apt-get remove build-essential fakeroot flex
+sudo apt autoremove
 sudo apt-get remove libssl-dev
 sudo apt-get remove lzma lzop bison libncurses5-dev:amd64 || echo "some error"
 else
@@ -17,6 +17,7 @@ else
 #http://cn.archive.ubuntu.com/ubuntu
 #http://mirror.lupaworld.com
 if [ ! -d bb-kernel ];then
+	git config --global http.postBuffer  524288000
 git clone https://github.com/RobertCNelson/bb-kernel.git
 fi
 cd bb-kernel/
