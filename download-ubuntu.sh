615583bcf9f83c07bce2cc5f5d41f84935cf0c8d
bonaventureli <lfwendula@msn.cn>
2018-08-25 21:38:23 +0800

    make-sdcard-ubuntu

--- download-ubuntu.sh
+++ download-ubuntu.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-
-
-wget http://releases.ubuntu.com/12.04/ubuntu-12.04.5-desktop-amd64.iso
-wget http://releases.ubuntu.com/12.04/ubuntu-12.04.5-desktop-i386.iso
-wget http://releases.ubuntu.com/12.04/ubuntu-12.04.5-server-amd64.iso
-wget http://releases.ubuntu.com/12.04/ubuntu-12.04.5-server-i386.iso
-
-
-
-wget http://releases.ubuntu.com/14.04/ubuntu-14.04.5-desktop-amd64.iso
-wget http://releases.ubuntu.com/14.04/ubuntu-14.04.5-desktop-i386.iso
-wget http://releases.ubuntu.com/14.04/ubuntu-14.04.5-server-amd64.iso
-wget http://releases.ubuntu.com/14.04/ubuntu-14.04.5-server-i386.iso
-
-wget http://releases.ubuntu.com/16.04/ubuntu-16.04.4-desktop-amd64.iso
-wget http://releases.ubuntu.com/16.04/ubuntu-16.04.4-desktop-i386.iso
-wget http://releases.ubuntu.com/16.04/ubuntu-16.04.4-server-amd64.iso
-wget http://releases.ubuntu.com/16.04/ubuntu-16.04.4-server-i386.iso
-
-wget http://releases.ubuntu.com/17.10/ubuntu-17.10.1-desktop-amd64.iso
-wget http://releases.ubuntu.com/17.10/ubuntu-17.10.1-server-amd64.iso
-wget http://releases.ubuntu.com/17.10/ubuntu-17.10.1-server-i386.iso
-
-wget http://releases.ubuntu.com/18.04/ubuntu-18.04-desktop-amd64.iso
-wget http://releases.ubuntu.com/18.04/ubuntu-18.04-live-server-amd64.iso
+#wget http://releases.ubuntu.com/12.04/ubuntu-12.04.5-desktop-amd64.iso
+#wget http://releases.ubuntu.com/12.04/ubuntu-12.04.5-desktop-i386.iso
+#wget http://releases.ubuntu.com/12.04/ubuntu-12.04.5-server-amd64.iso
+#wget http://releases.ubuntu.com/12.04/ubuntu-12.04.5-server-i386.iso
+#
+#wget http://releases.ubuntu.com/14.04/ubuntu-14.04.5-desktop-amd64.iso
+#wget http://releases.ubuntu.com/14.04/ubuntu-14.04.5-desktop-i386.iso
+#wget http://releases.ubuntu.com/14.04/ubuntu-14.04.5-server-amd64.iso
+#wget http://releases.ubuntu.com/14.04/ubuntu-14.04.5-server-i386.iso
+#
+#wget http://releases.ubuntu.com/16.04/ubuntu-16.04.4-desktop-amd64.iso
+#wget http://releases.ubuntu.com/16.04/ubuntu-16.04.4-desktop-i386.iso
+#wget http://releases.ubuntu.com/16.04/ubuntu-16.04.4-server-amd64.iso
+#wget http://releases.ubuntu.com/16.04/ubuntu-16.04.4-server-i386.iso
+#
+#wget http://releases.ubuntu.com/17.10/ubuntu-17.10.1-desktop-amd64.iso
+#wget http://releases.ubuntu.com/17.10/ubuntu-17.10.1-server-amd64.iso
+#wget http://releases.ubuntu.com/17.10/ubuntu-17.10.1-server-i386.iso
+#
+#wget http://releases.ubuntu.com/18.04/ubuntu-18.04-desktop-amd64.iso
+#wget http://releases.ubuntu.com/18.04/ubuntu-18.04-live-server-amd64.iso
+#mkfs.vat /dev/sd*
+#dd if=ubuntu......iso of=/dev/sd*
