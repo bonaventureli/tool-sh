5e27f01349cb8c2ce51c9335eb4b73b8cf3d4700
bonaventureli <lfwendula@msn.cn>
2019-02-13 19:37:40 +0800

    update

---
+++ download-android-studio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget -c https://dl.google.com/dl/android/studio/ide-zips/3.1.2.0/android-studio-ide-173.4720617-linux.zip
+#wget -c https://dl.google.com/dl/android/studio/install/3.1.2.0/android-studio-ide-173.4720617-windows.exe
+#wget -c https://dl.google.com/dl/android/studio/ide-zips/3.1.2.0/android-studio-ide-173.4720617-windows.zip
+#wget -c https://dl.google.com/dl/android/studio/ide-zips/3.1.2.0/android-studio-ide-173.4720617-windows32.zip
+#wget -c https://dl.google.com/android/repository/sdk-tools-windows-3859397.zip
+#wget -c https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
---
+++ download-teamviewer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#wget -c https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+#wget -c https://download.teamviewer.com/download/linux/teamviewer_i386.deb
+#wget -c https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
+#wget -c https://download.teamviewer.com/download/linux/teamviewer.i686.rpm
+#wget -c https://download.teamviewer.com/download/linux/teamviewer-suse.x86_64.rpm
+#wget -c https://download.teamviewer.com/download/linux/teamviewer-suse.i686.rpm
+wget -c https://download.teamviewer.com/download/linux/teamviewer_amd64.tar.xz
+#wget -c https://download.teamviewer.com/download/linux/teamviewer_i386.tar.xz
