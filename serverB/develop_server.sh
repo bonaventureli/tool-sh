c30892b877af4aac7942cf1114ce65126c1bd000
bonaventureli <lfwendula@msn.cn>
2019-05-03 09:36:55 +0800

    add y410p grub

--- serverB/develop_server.sh
+++ serverB/develop_server.sh
@@ -5,3 +5,7 @@ Y410p
 vim /etc/apache2/prots.conf
 vim /etc/apache2/sites-enabled/000-default.conf
 /etc/init.d/apache2 restart
+set root=(hd1,msdos1)
+set prefix=(hd1,msdos1)/boot/grub
+insmod normal
+normal
