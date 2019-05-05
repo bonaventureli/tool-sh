a391936430b31bd557152775d9ec2e7430a99cbf
bonaventureli <lfwendula@msn.cn>
2019-05-06 07:32:41 +0800

    update

--- serverB/develop_server.sh
+++ serverB/develop_server.sh
@@ -2,23 +2,16 @@
 vim /etc/hostname
 V470c
 Y410p
-<<<<<<< HEAD
 Tcloud
 vim /etc/resolv.conf 
 dig www.baidu.com
 vim /etc/hosts #change hosts name
 #118.25.108.6 Tcloud
 #192.168.1.8 Y410P
-
 hostname
 curl getip.name #check public ip
-
 vim /etc/sudoers
 #youuser	ALL=(ALL)	ALL
-
-
-
-=======
 vim /etc/apache2/prots.conf
 vim /etc/apache2/sites-enabled/000-default.conf
 /etc/init.d/apache2 restart
@@ -27,4 +20,3 @@ set prefix=(hd1,msdos1)/boot/grub
 insmod normal
 normal
 vim /etc/apache2/apache2.conf
->>>>>>> b43db2bb4c8a0bac3b9a23a7b87acd57cde60ab8
