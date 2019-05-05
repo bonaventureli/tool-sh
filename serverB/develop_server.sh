9d30020659c993b74b3159955010a0dfcb4df39c
bonaventureli <lfwendula@msn.cn>
2019-05-06 07:24:09 +0800

    update

--- serverB/develop_server.sh
+++ serverB/develop_server.sh
@@ -2,4 +2,18 @@
 vim /etc/hostname
 V470c
 Y410p
+Tcloud
+vim /etc/resolv.conf 
+dig www.baidu.com
+vim /etc/hosts #change hosts name
+#118.25.108.6 Tcloud
+#192.168.1.8 Y410P
+
+hostname
+curl getip.name #check public ip
+
+vim /etc/sudoers
+#youuser	ALL=(ALL)	ALL
+
+
 
